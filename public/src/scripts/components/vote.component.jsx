7061f1cba7ee4dbf5217f6c8947b9f421d65487c
indigo-carie <carie.phon@indigoslate.com>
2017-03-13 16:08:29 -0700

    added some sass files

--- public/src/scripts/components/vote.component.jsx
+++ public/src/scripts/components/vote.component.jsx
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import Song from './song.component.jsx';
 
 class Vote extends Component {
 
+	constructor(){
+		super();
+		this.songs = ['Song A','Song B'];
+	}
+
 	render(){
+		var votingButtons = this.songs.map((song,i)=>{
+			return <button className={song} key={i}>{song}</button>
+		});
+
 		return(
-			<h1>Vote!</h1>
-			)
+			<div>
+				<h1>Vote!</h1>
+				{ votingButtons }
+			</div>
+		)
 	}
 
 }
