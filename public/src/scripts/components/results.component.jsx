51e14f907ecbf1b7ba3e471b8e05fb5d68bd1f6d
gloomyTofu <hello@cariephon.com>
2017-03-16 12:43:49 -0700

    added svg context to results.component wip

--- public/src/scripts/components/results.component.jsx
+++ public/src/scripts/components/results.component.jsx
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 class Results extends Component {
+  componentDidMount() {
+    this.setContent();
+  }
+
+  setContext() {
+  
+  }
+
   render() {
     return (
       <svg className="visualization bubbles"></svg>
